4757f1bc771a7f357dbf463229edf9df6ac4a928
David Adamashvili <david.adamashvili71@gmail.com>
2022-01-18 01:41:05 +0400

    re-fixed window sizing

--- script.js
+++ script.js
@@ -4,8 +4,16 @@ window.onload = () => {
 	/** @type {HTMLCanvasElement} */
 	const canvas = document.getElementsByClassName("snake-game")[0];
 
+	const borderWidth = 2;
+
 	canvas.width = canvas.height =
-		Math.min(window.innerHeight, window.innerWidth) / 1.5;
+		(Math.min(document.body.clientHeight, document.body.clientWidth) -
+			borderWidth) /
+		1.5;
+
+	console.log(Math.min(window.innerHeight, window.innerWidth));
+	console.log(window.innerWidth, window.innerHeight);
+	console.log(canvas.width, canvas.height);
 
 	const options = {
 		canvas,
